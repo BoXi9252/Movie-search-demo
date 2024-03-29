cafc4729a7d95688bcb30ea2571e04d55bf9ef7c
BoXi9252 <zhaoyanglove2022@126.com>
2024-03-29 17:29:58 -0500

    style

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,14 +1,13 @@
 import movies from './movies.js';
 
-// 你的app.js中的其他代码...
 
-// 获取页面元素
+// 获取页面元素: get elements 
 const searchInput = document.querySelector('.movie-search-input');
 const matchedMoviesDisplay = document.querySelector('.matched-movies-wrapper ul');
 const findButton = document.querySelector('.find-button');
 const movieContainer = document.querySelector('.movie-information-container');
 
-// 搜索电影并更新匹配列表
+// 搜索电影并更新匹配列表:find movie list
 function listMovies(input) {
     if (input.length < 3) {
         matchedMoviesDisplay.innerHTML = '';
@@ -26,7 +25,7 @@ function listMovies(input) {
             newLi.textContent = occurence;
             matchedMoviesDisplay.appendChild(newLi);
 
-            // 当点击搜索建议时，将文本复制到输入框并清空列表
+            // 当点击搜索建议时，将文本复制到输入框并清空列表:click the search suggestions, and copy the query text to the input
             newLi.addEventListener('click', () => {
                 searchInput.value = occurence;
                 matchedMoviesDisplay.innerHTML = '';
@@ -37,7 +36,7 @@ function listMovies(input) {
     }
 }
 
-// 根据搜索输入显示电影信息
+// 根据搜索输入显示电影信息: show the movie information
 function getMovie() {
     const movieFound = movies.find(movie => movie.title.trim().toLowerCase() === searchInput.value.trim().toLowerCase());
 
@@ -67,7 +66,7 @@ function getMovie() {
     }
 }
 
-// 添加事件监听器
+// 添加事件监听器: addEventListener:input \click\
 searchInput.addEventListener('input', () => listMovies(searchInput.value));
 findButton.addEventListener('click', getMovie);
 document.querySelector('form').addEventListener('submit', event => event.preventDefault());
